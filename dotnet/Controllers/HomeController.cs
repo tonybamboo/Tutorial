dc4cd75fd2f0a87cdac8f71a338be9f2552896f2
tonybamboo <tonybamboo@gmail.com>
2017-04-10 15:06:46 +0100

    Renoved home number

---
+++ dotnet/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace dotnet.Controllers
+
+{
+    
+
+public class HomeController : Controller
+    
+{
+ 
+
+    public HomeController()
+    {
+    }
+
+    public IActionResult Index()
+
+    {
+
+    return View();
+
+    }
+
+        
+
+    public IActionResult About()
+    {
+	ViewData["Message"] = "Your application description page.";
+
+           
+
+ 	return View();
+        }
+
+    
+
+    public IActionResult Contact()
+
+        {
+           
+ ViewData["Message"] = "Your contact page.";
+
+   
+         return View();
+        }
+
+          
+
+
+ public IActionResult Profile()
+        {
+  
+          ViewData["Message"] = "Tony Greenhalgh profile.";
+
+      
+      return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
+
